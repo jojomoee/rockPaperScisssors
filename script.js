823ec9b29fa69070e39ec08802128a5cedb080f7
Johannes Moe <jojoomoe@protonmail.com>
2022-02-26 00:41:58 +0100

    save

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnRock = document.getElementById("rock-btn");
+let btnPaper = document.getElementById("paper-btn");
+let btnScissors = document.getElementById("scisscors-btn");
+
+let result = document.getElementById("result");
+
+const choices = ["rock", "paper", "scissor"];
