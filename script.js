eefce763ac4fa042885b769a9b3c94873da5f1bb
Johannes Moe <jojoomoe@protonmail.com>
2022-02-26 20:10:16 +0100

    finito

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-let btnRock = document.getElementById("rock-btn");
-let btnPaper = document.getElementById("paper-btn");
-let btnScissors = document.getElementById("scisscors-btn");
+const choicesBtn = document.querySelectorAll(".choice-btn");
+const result = document.getElementById("result");
 
-let result = document.getElementById("result");
+choicesBtn.forEach((choice) => {
+  choice.addEventListener("click", function () {
+    const playerInput = this.textContent;
+    const computerOptions = ["Rock", "Paper", "Scissors"];
+    const computerChoice = computerOptions[Math.floor(Math.random() * 3)];
 
-const choices = ["rock", "paper", "scissor"];
+    const resultOfMatch = `${playerInput} vs ${computerChoice}`;
+
+    //function(playerInput, computerChoice); funktioniert nicht ??
+
+    if (playerInput === computerChoice) {
+      result.innerHTML = `${resultOfMatch} it's a Tie`;
+      return;
+    }
+
+    if (playerInput === "Rock") {
+      if (computerChoice === "Scissors") {
+        result.innerHTML = `${resultOfMatch} You win`;
+      } else {
+        result.innerHTML = `${resultOfMatch} You loose`;
+      }
+    } else if (playerInput === "Paper") {
+      if (computerChoice === "Rock") {
+        result.innerHTML = `${resultOfMatch} You win`;
+      } else {
+        result.innerHTML = `${resultOfMatch} You loose`;
+      }
+    } else if (playerInput === "Scissors") {
+      if (computerChoice === "Papaer") {
+        result.innerHTML = `${resultOfMatch} You win`;
+      } else {
+        result.innerHTML = `${resultOfMatch} You loose`;
+      }
+    }
+  });
+});
